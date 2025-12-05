83553f56ee8bc3f3ce9e4a2b7428fd99e61d4be5
dpiltzVFS <dpiltz@vfs.com>
2025-12-05 14:56:46 -0800

    Added support for jumping and landing to the FMODFootsteps script

--- Runtime/FMODFootsteps.cs
+++ Runtime/FMODFootsteps.cs
@@ -5,15 +5,47 @@ namespace FMODExtensions
     public class FMODFootsteps : MonoBehaviour
     {
         [SerializeField] public FMODUnity.EventReference _footstepEvent;
+        [SerializeField] public FMODUnity.EventReference _jumpEvent;
+        [SerializeField] public FMODUnity.EventReference _landEvent;
 
         public virtual void PlayFootstep(RaycastHit hit, float speed)
         {
-            FMOD.Studio.EventInstance instance = FMODUnity.RuntimeManager.CreateInstance(_footstepEvent);
-            instance.setParameterByName("Material", GetSurfaceMaterial(hit));
-            instance.setParameterByName("Speed", speed);
-            instance.start();
-            instance.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(transform.position));
-            instance.release();
+            if (!_footstepEvent.IsNull)
+            {
+                FMOD.Studio.EventInstance instance = FMODUnity.RuntimeManager.CreateInstance(_footstepEvent);
+                instance.setParameterByName("Material", GetSurfaceMaterial(hit));
+                instance.setParameterByName("Speed", speed);
+                instance.start();
+                instance.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(transform.position));
+                instance.release();
+            }
+        }
+
+        public virtual void PlayJump(RaycastHit hit, float speed)
+        {
+            if (!_jumpEvent.IsNull)
+            {
+
+                FMOD.Studio.EventInstance instance = FMODUnity.RuntimeManager.CreateInstance(_jumpEvent);
+                instance.setParameterByName("Material", GetSurfaceMaterial(hit));
+                instance.setParameterByName("Speed", speed);
+                instance.start();
+                instance.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(transform.position));
+                instance.release();
+            }
+        }
+
+        public virtual void PlayLand(RaycastHit hit, float speed)
+        {
+            if (!_landEvent.IsNull)
+            {
+                FMOD.Studio.EventInstance instance = FMODUnity.RuntimeManager.CreateInstance(_landEvent);
+                instance.setParameterByName("Material", GetSurfaceMaterial(hit));
+                instance.setParameterByName("Speed", speed);
+                instance.start();
+                instance.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(transform.position));
+                instance.release();
+            }
         }
 
         // get surface material value from GameObject
@@ -22,5 +54,7 @@ namespace FMODExtensions
             if (hit.collider != null && hit.collider.gameObject.TryGetComponent(out FMODSurface surface) && surface.SurfaceMaterial != null) return surface.SurfaceMaterial.SurfaceValue;
             return 0f;
         }
+
+
     }
 }
