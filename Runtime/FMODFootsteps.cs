9e37e693a098bfa462dfe13105e8f779e3d4f7a2
dpiltzVFS <dpiltz@vfs.com>
2025-12-10 09:15:04 -0800

    Added option to print FMOD material ID to the console and exposed the FMOD Parameter Strings in the inspector so they can be changed is needed

--- Runtime/FMODFootsteps.cs
+++ Runtime/FMODFootsteps.cs
@@ -8,13 +8,18 @@ namespace FMODExtensions
         [SerializeField] public FMODUnity.EventReference _jumpEvent;
         [SerializeField] public FMODUnity.EventReference _landEvent;
 
+        [SerializeField] public string FMODMaterialParameterName = "Material";
+        [SerializeField] public string FMODMCharacterSpeedParameterName = "LocalPlayerSpeed";
+
+        [SerializeField] public bool PrintFloorSurfaceMaterialID = false;
+
         public virtual void PlayFootstep(GameObject floorObject, bool overTerrain, float speed)
         {
             if (!_footstepEvent.IsNull)
             {
                 FMOD.Studio.EventInstance instance = FMODUnity.RuntimeManager.CreateInstance(_footstepEvent);
-                instance.setParameterByName("Material", GetSurfaceMaterial(floorObject));
-                instance.setParameterByName("Speed", speed);
+                instance.setParameterByName(FMODMaterialParameterName, GetSurfaceMaterial(floorObject));
+                instance.setParameterByName(FMODMCharacterSpeedParameterName, speed);
                 instance.start();
                 instance.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(transform.position));
                 instance.release();
@@ -27,8 +32,8 @@ namespace FMODExtensions
             {
 
                 FMOD.Studio.EventInstance instance = FMODUnity.RuntimeManager.CreateInstance(_jumpEvent);
-                instance.setParameterByName("Material", GetSurfaceMaterial(floorObject));
-                instance.setParameterByName("Speed", speed);
+                instance.setParameterByName(FMODMaterialParameterName, GetSurfaceMaterial(floorObject));
+                instance.setParameterByName(FMODMCharacterSpeedParameterName, speed);
                 instance.start();
                 instance.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(transform.position));
                 instance.release();
@@ -40,8 +45,8 @@ namespace FMODExtensions
             if (!_landEvent.IsNull)
             {
                 FMOD.Studio.EventInstance instance = FMODUnity.RuntimeManager.CreateInstance(_landEvent);
-                instance.setParameterByName("Material", GetSurfaceMaterial(floorObject));
-                instance.setParameterByName("Speed", speed);
+                instance.setParameterByName(FMODMaterialParameterName, GetSurfaceMaterial(floorObject));
+                instance.setParameterByName(FMODMCharacterSpeedParameterName, speed);
                 instance.start();
                 instance.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(transform.position));
                 instance.release();
@@ -50,7 +55,14 @@ namespace FMODExtensions
 
         public float GetSurfaceMaterial(GameObject floorObject)
         {
-            if (floorObject.TryGetComponent(out FMODSurface surface) && surface.SurfaceMaterial != null) return surface.SurfaceMaterial.SurfaceValue;
+            if (floorObject.TryGetComponent(out FMODSurface surface) && surface.SurfaceMaterial != null)
+            {
+                if (PrintFloorSurfaceMaterialID)
+                {
+                    Debug.Log("MaterialID = " + surface.SurfaceMaterial.SurfaceValue);
+                    return surface.SurfaceMaterial.SurfaceValue;
+                }
+            }
             return 0f;
         }
     }
